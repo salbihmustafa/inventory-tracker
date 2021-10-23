dc889402483a255022b55c8299a0c92498ae9ff9
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-10-23 17:53:43 -0500

    npm install firebase and added requireAuth inside index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../views/auth/Signup.vue'
+
+//route guard
+import { projectAuth } from '../firebase/config';
+
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if(!user){
+    //if no user
+    next({ name: 'Login' }); //direct them to the Login page
+  }else {
+    next(); //if user exists then allow them to go where they need to
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: requireAuth
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   },
 ]
 
