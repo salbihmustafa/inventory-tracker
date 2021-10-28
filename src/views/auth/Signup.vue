8fbc5b653b975aeb8c418980276a81486087d3f5
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-10-27 19:13:18 -0500

    install material icons and add visibility eye icon to password input

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,31 +1,79 @@
 <template>
-  <form>
-      <h3>Sign up</h3>
-      <input type="text" placeholder="Display Name" v-model="displayName">
-      <input type="email" placeholder="Email" v-model="email">
-      <input type="password" placeholder="Password" v-model="password">
-      <button>Sign Up</button>
+  <form @submit.prevent="handleSubmit">
+    <h3>Sign up</h3>
+    <input type="text" placeholder="Display Name" v-model="displayName" />
+    <input type="email" placeholder="Email" v-model="email" />
+    <input :type="passtype" placeholder="Password" v-model="password" />
+    <div v-if="!visible">
+      <span class="material-icons" @click="toggleVisibility">visibility_off</span>
+    </div>
+    <div v-if="visible">
+      <span class="material-icons" @click="toggleVisibility">visibility</span>
+    </div>
+    <div class="error" v-if="error">{{ error }}</div>
+    <button v-if="!isPending">Sign Up</button>
+    <button v-if="isPending" disabled>Loading..</button>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import useSignup from "@/composables/useSignup.js";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-  setup(){
-    const displayName = ref('');
-    const email = ref('');
-    const password = ref('');
+  data(){
+    return {
+      visible: false, //toggle to show or hide password
+      passtype: 'password',
+    }
+  },
+  setup() {
+    const { error, signup, isPending } = useSignup();
+    const router = useRouter();
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
 
-    return { displayName, email, password }
+    const handleSubmit = async () => {
+      const response = await signup(
+        email.value,
+        password.value,
+        displayName.value
+      );
+      if (!error.value) {
+        //enters here when there are no errors
+        console.log("User successfully signed up");
+        //Route to home page
+        router.push({ name: "Home" });
+      }
+    };
+
+    return { displayName, email, password, handleSubmit, error, isPending };
+  },
+  methods: {
+    toggleVisibility () {
+      this.visible = !this.visible;
+      if(this.visible){
+        //show password
+        this.passtype = 'text';
+      }else{
+        //hide password
+        this.passtype = 'password';
+      }
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-h3{
+h3 {
   text-align: center;
 }
-
+span {
+  position: relative;
+  top: -50px;
+  left: 375px;
+  cursor: pointer;
+}
 </style>
