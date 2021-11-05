afb046aee27112dfa716f9c511ab27c644967289
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-11-04 19:27:31 -0500

    removed signup and login buttons in the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,35 +1,10 @@
 <template>
-  <!-- <div class="navbar">
-    <nav>
-      <h1><router-link :to="{ name: 'Home' }">Inventory List</router-link></h1>
-      <div class="links">
-        <div v-if="user">
-          <router-link :to="{ name: 'CreatePlaylist'}">Create Playlist</router-link>
-          <router-link :to="{ name: 'UserPlaylists'}">My Playlist</router-link>
-          <span>Hi, {{ user.displayName }}</span>
-          <button @click.prevent="handleLogout">Logout</button>
-        </div>
-        <div v-else>
-          <router-link class="btn" :to="{ name: 'Signup' }"
-            >Signup</router-link
-          >
-          <router-link class="btn" :to="{ name: 'Login' }"
-            >Log in</router-link
-          >
-        </div>
-      </div>
-    </nav>
-  </div> -->
   <div class="navbar">
     <nav>
       <h1><router-link :to="{ name: 'Home' }">Inventory List</router-link></h1>
       <div class="links">
         <div v-if="user">
           <button @click.prevent="handleLogout">Logout</button>
-        </div>
-        <div v-else>
-          <router-link class="btn" :to="{ name: 'Signup' }">Sign up</router-link>
-          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
         </div>
       </div>
     </nav>
@@ -63,9 +38,6 @@ export default {
 </script>
 
 <style scoped>
-.navbar {
-  /* border: 1px solid red; */
-}
 nav {
   display: flex;
   justify-content: space-between;
