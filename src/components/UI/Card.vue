6b0d45a5ae3c6ff374f113e28d8e151d2fc2407b
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-11-06 16:28:35 -0500

    added a card and created dummy data within home.vue

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <div>Test</div>
+    <div>Test</div>
+    <div>Test</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [],
+    components: {},
+    setup(){
+        
+    }
+};
+</script>
+
+<style scoped>
+.card {
+  margin: 10px 0;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 10px 20px 20px 20px rgba(50, 50, 50, 0.3);
+  background: #fff;
+  border: 1px solid var(--secondary);
+  cursor: pointer;
+
+  display: flex;
+}
+.card div{
+    flex: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,49 @@
 <template>
   <div>
     <h1>Inventory List</h1>
+    <Card v-for="item in items" :key="item.id" :data="items" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Card from "@/components/UI/Card";
+import { ref } from "vue";
+import { Guid } from "js-guid";
 
 export default {
   name: "Home",
-  components: {  },
+  components: { Card },
+  setup() {
+    const items = ref([]);
+    let item1 = {
+      title:
+        "Rebecca Vallance Women's Sz 2 Billie Ruffle Ice Blue Off The Shoulder Mini Dress",
+      priceListed: 170,
+      paid: 41.62,
+      fiftyPercent: 104.05,
+      sku: "B6",
+      weight: "2 lbs",
+      dateListed: "10/25/2021",
+      dom: 12,
+      id: Guid.newGuid().toString(),
+    };
+    let item2 = {
+      title:
+        "Alexis Women's Sz XS Faine Floral Hawaiian V-Neck Ruffle Calipso Red Romper",
+      priceListed: 170,
+      paid: 41.62,
+      fiftyPercent: 104.05,
+      sku: "B6",
+      weight: "2 lbs",
+      dateListed: "10/25/2021",
+      dom: 12,
+      id: Guid.newGuid().toString(),
+    };
+    items.value.push(item1);
+    items.value.push(item2);
+
+    return { items };
+  },
 };
 </script>
