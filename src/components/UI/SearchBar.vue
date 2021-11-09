27cf6ef26a0d911822a006af9a60119336e9b787
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-11-09 11:12:56 -0600

    added searchbar html and css

--- src/components/UI/SearchBar.vue
+++ src/components/UI/SearchBar.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>Search Bar</div>
+  <div class="ui segment">
+    <form class="ui form" @submit.prevent="handleSubmit">
+      <div class="ui icon input">
+        <input type="text" placeholder="Search..." v-model="searchValue" />
+        <i class="circular search link icon"></i>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
+import { ref } from "vue";
 
-}
+export default {
+  setup() {
+    const searchValue = ref("");
+
+    const handleSubmit = () => {
+      console.log(searchValue.value);
+      searchValue.value = ""; //reset search bar
+    };
+
+    return { searchValue, handleSubmit };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.ui.form {
+  box-shadow: 5px 5px 10px 10px rgba(50, 50, 50, 0.3);
+  display: flex;
+}
+.ui.icon.input {
+  /* border: 1px solid red; */
+  flex: 1;
+}
 </style>
